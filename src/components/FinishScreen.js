c77ba868cf2a3b391f98adb9e3da7dc2194868f5
Sam Mariassouce <sammariassouce@gmail.com>
2023-08-14 10:36:48 -1000

    198

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,22 @@
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = '🥇';
+  if (percentage >= 80 && percentage < 100) emoji = '🥈';
+  if (percentage >= 50 && percentage < 80) emoji = '🥉';
+  if (percentage >= 0 && percentage < 50) emoji = '📜';
+  if (percentage >= 0 && percentage < 50) emoji = '🎓';
+  if (percentage === 0) emoji = '😜';
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong> {points}</strong> out of
+        <span> {maxPossiblePoints}</span>({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(highscore: {highscore} points)</p>
+    </>
+  );
+}
+
+export default FinishScreen;
