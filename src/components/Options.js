8675afbffbb2762166a2e24dc8753eb4fa2b591a
Sam Mariassouce <sammariassouce@gmail.com>
2023-08-14 07:36:01 -1000

    195

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,21 @@
-function Option({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? 'answer' : ''} 
+          ${
+            hasAnswered
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          key={option}
+          disabled={answer !== null}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+        >
           {option}
         </button>
       ))}
@@ -10,4 +23,4 @@ function Option({ question }) {
   );
 }
 
-export default Option;
+export default Options;
